248ad119b7cc6f22d652fe93c529b8d517d9393c
Rahul Singh <rahul.singh.ec.2017@miet.ac.in>
2023-02-13 18:20:28 +0530

    routing added and most component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { CloudComponent } from './components/cloud/cloud.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { HomeComponent } from './components/home/home.component';
+import { ITComponent } from './components/it/it.component';
+import { ReviewComponent } from './components/review/review.component';
+import { StaffingComponent } from './components/staffing/staffing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'aboutus',
+    component: AboutusComponent
+  },
+  {
+    path: 'gallery',
+    component: GalleryComponent
+  },
+  {
+    path:'blog',
+    component: BlogComponent
+  },
+  {
+    path:'review',
+    component:ReviewComponent
+  },
+  {
+    path:'contact',
+    component: ContactComponent
+  },
+  {
+    path:"IT",
+    component: ITComponent
+  },
+  {
+    path: 'cloud',
+    component: CloudComponent
+  },
+  {
+    path:'staffing',
+    component: StaffingComponent
+  },
+  {
+    path: 'courses',
+    component: CoursesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ReviewComponent } from './components/review/review.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ITComponent } from './components/it/it.component';
+import { CloudComponent } from './components/cloud/cloud.component';
+import { StaffingComponent } from './components/staffing/staffing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutusComponent,
+    GalleryComponent,
+    BlogComponent,
+    ReviewComponent,
+    ContactComponent,
+    ITComponent,
+    CloudComponent,
+    StaffingComponent
   ],
   imports: [
     BrowserModule,
